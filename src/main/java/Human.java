2319a61fddbd39d9205be29aaf99b9a957922bf3
Terikbaev.A <a.terikbaev16@mail.ru>
2024-03-28 12:27:47 +0300

    first commit

--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Human {
+public class Human {
+    public static void main(String[] args) {
+        Person person1 = new Person(18, "bob");
+        person1.getPension();
+    }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Person {
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(int age, String name) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void getPension() {
+        if (this.age > 65) {
+            System.out.println("Your age is " + this.age + ", you need to go pension");
+        } else {
+            System.out.println("Your age is " + this.age + ", you not need to go pension");
+        }
+    }
 }
